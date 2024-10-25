4e5e626bf29a9d71b834632ac6185da77223ba96
Fareed Elbokl <fareed.elbokl@gmail.com>
2024-10-25 14:51:15 -0400

    Added comments

--- process1.c
+++ process1.c
@@ -11,7 +11,8 @@ void sem_lock(int semid);
 void sem_unlock(int semid);
 
 int main() {
-    key_t key = 1234; // Shared memory key
+    // Semaphore: Essentially before a process does anything to shared memory it first locks (decrements) the semaphore, but if another process currently has the semaphore locked, then it waits until that process unlocks (increments) the semaphore
+    key_t key = 1234;     // Shared memory key
     key_t sem_key = 5678; // Semaphore key
     int shmid, semid;
     int *shared_var;
@@ -31,14 +32,14 @@ int main() {
     }
 
     // Create a semaphore set with 1 semaphore
-    semid = semget(sem_key, 1, IPC_CREAT | 0666);
+    semid = semget(sem_key, 1, IPC_CREAT | 0666); // Pass in the sem key, number of sems, and sem flag
     if (semid < 0) {
         perror("semget failed");
         exit(EXIT_FAILURE);
     }
 
     // Initialize the semaphore to 1
-    if (semctl(semid, 0, SETVAL, 1) < 0) {
+    if (semctl(semid, 0, SETVAL, 1) < 0) { // semctl used to perform initialization and deletion operations on semaphores
         perror("semctl failed");
         exit(EXIT_FAILURE);
     }
@@ -54,7 +55,7 @@ int main() {
         int random_value = rand() % 11;
 
         // Lock the semaphore to protect shared memory access
-        sem_lock(semid);
+        sem_lock(semid); // This means process 1 has exclusive access to the shared memory, and other processes must wait until semaphore is unlocked
         *shared_var = random_value; // Update the shared memory variable
         printf("I am Process 1. Generated value: %d\n", *shared_var);
         sem_unlock(semid); // Unlock the semaphore
@@ -105,12 +106,12 @@ int main() {
 
 // Function to lock the semaphore (decrement)
 void sem_lock(int semid) {
-    struct sembuf sb = {0, -1, 0};
+    struct sembuf sb = {0, -1, 0}; // Middle value specifies the semaphore operation to be done, here we decrement to 0 as we want to lock the semaphore
     semop(semid, &sb, 1);
 }
 
 // Function to unlock the semaphore (increment)
 void sem_unlock(int semid) {
-    struct sembuf sb = {0, 1, 0};
+    struct sembuf sb = {0, 1, 0}; // Middle value specifies
     semop(semid, &sb, 1);
 }
